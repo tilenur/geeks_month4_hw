9144bb27d85493e04a748fe49f7f95823d8019ac
Nurbek Tilebaliev <nurbektilebaliev@Nurbeks-MacBook-Pro.local>
2025-05-08 17:52:15 +0900

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -16,16 +16,64 @@ gmailButton.onclick = () => {
   }
 };
 
-//MOVE BLOCK
+//MOVE BLOCK FULLY
 
-let blockMove = document.querySelector(".child_block");
-let positionX = 0;
+const parentBlock = document.querySelector(".parent_block");
+let childBlock = document.querySelector(".child_block");
+
+let positionX = 0,
+  positionY = 0;
+
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight;
 
 const movingBlock = () => {
-  blockMove.style.left = positionX + "px";
-  positionX++;
-  if (positionX < 450) {
-    requestAnimationFrame(movingBlock);
-  }
+  requestAnimationFrame(movingBlock);
+  childBlock.style.left = `${positionX}px`;
+  childBlock.style.top = `${positionY}px`;
+
+  if (positionX < offsetWidth && positionY === 0) positionX++; //move right
+  else if (positionX >= offsetWidth && positionY < offsetHeight)
+    positionY++; //move down
+  else if (positionY >= offsetHeight && positionX > 0) positionX--; //moves left
+  else if (positionX <= 0 && positionY > 0) positionY--; //moves up
 };
 movingBlock();
+
+//Stopwatch
+
+const seconds = document.querySelector("#seconds");
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetbtn = document.querySelector("#reset");
+
+let num = 0;
+let intervalId = null;
+
+// Start btn
+startBtn.onclick = () => {
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      num++;
+      seconds.innerHTML = num;
+    }, 1000);
+  }
+};
+
+// Stop Btn
+stopBtn.onclick = () => {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+};
+
+//Reset btn
+resetbtn.onclick = () => {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+  num = 0;
+  seconds.innerHTML = num;
+};
